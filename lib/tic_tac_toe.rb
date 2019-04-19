47dcacdaac9b7bffe867a7165b270ae29c6a2bd8
Anthony Rurup <tcrurup@gmail.com>
2019-04-19 19:19:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,3 +49,11 @@ end
 def turn_count(board)
   board.reject{|char| char == " "  || char == nil}.length
 end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    'X'
+  else
+    'O'
+  end
+end
