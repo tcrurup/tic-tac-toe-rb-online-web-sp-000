fed386701325346c40ba2b2e915d2f34e9c14ff0
Anthony Rurup <tcrurup@gmail.com>
2019-04-19 19:02:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,3 +28,7 @@ end
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+
+def valid_move?(board, index)
+  !position_taken?(board, index) && index.between?(0,8)
+end
