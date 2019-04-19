8373bb8b6a736baef5dc2920ca36fef193520d86
Anthony Rurup <tcrurup@gmail.com>
2019-04-19 18:56:57 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,10 @@
+WIN_COMBINATIONS = [
+  [0,1,2], #top row 
+  [3,4,5], #middle row
+  [6,7,8], #bottom row
+  [0,3,6], #first column
+  [1,4,7], #second column
+  [2,5,8], #third column
+  [6,4,2], #diagonal foward slash
+  [0,4,8], #diagonal back slash
+]
